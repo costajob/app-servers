63fe2d3ef3a1b6b43b5e2a4fc6872235a746a186
Zachary Anderson <zanderso@users.noreply.github.com>
2017-10-03 12:20:28 -0700

    Update dart_server.dart
    Use 4 Isolates. Also avoid a copy of the greeting on every request.

--- servers/dart_server.dart
+++ servers/dart_server.dart
@@ -1,15 +1,27 @@
+import 'dart:convert';
 import 'dart:io';
+import 'dart:isolate';
 
 const int _PORT = 9292;
 const String _GREET = 'Hello World';
 
-void main() {
-  HttpServer.bind(InternetAddress.ANY_IP_V4, _PORT).then((server) { 
+_startServer(arg) {
+  List<int> response = UTF8.encode(_GREET);
+  HttpServer.bind(InternetAddress.LOOPBACK_IP_V4, _PORT, shared:true).then((server) {
     server.listen((HttpRequest request) {
       request.response
         ..headers.contentType = new ContentType('text', 'plain')
-        ..write(_GREET)
+        ..add(response)
         ..close();
     });
   });
 }
+
+void main() {
+  final isolates = 4;
+  for (int i = 0; i < isolates - 1; i++) {
+    Isolate.spawn(_startServer, null);
+  }
+  _startServer(null);
+}
+
