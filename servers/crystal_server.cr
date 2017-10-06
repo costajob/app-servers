92f0c2e169449053c7d04d4a5dac1510b9ad7a8b
Aurimas Niekis <aurimas@niekis.lt>
2017-10-06 12:40:36 +0100

    Added Simple parallelism to the Crystal server example

--- servers/crystal_server.cr
+++ servers/crystal_server.cr
@@ -1,6 +1,24 @@
 require "http/server"
 
-HTTP::Server.new("0.0.0.0", 9292) do |context|
+NUM_OF_WORKERS = 8
+
+server = HTTP::Server.new("0.0.0.0", 9292) do |context|
   context.response.content_type = "text/plain"
   context.response.print "Hello World"
-end.listen
+end
+
+server.bind
+
+workers = [] of Process
+
+NUM_OF_WORKERS.times do
+  worker = fork do
+    server.listen
+  end
+
+  workers << worker
+end
+
+workers.each do |worker|
+  worker.wait
+end
