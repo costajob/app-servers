907bc07400aa4a4269411604a3aa6d4d06fa69d7
costajob <costajob@gmail.com>
2017-10-06 14:25:17 +0200

    Mike - reverted Crystal server to single process implementation

--- servers/crystal_server.cr
+++ servers/crystal_server.cr
@@ -1,24 +1,8 @@
 require "http/server"
 
-NUM_OF_WORKERS = 8
-
 server = HTTP::Server.new("0.0.0.0", 9292) do |context|
   context.response.content_type = "text/plain"
   context.response.print "Hello World"
 end
-
-server.bind
-
-workers = [] of Process
-
-NUM_OF_WORKERS.times do
-  worker = fork do
-    server.listen
-  end
-
-  workers << worker
-end
-
-workers.each do |worker|
-  worker.wait
-end
+  
+server.listen
