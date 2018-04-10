2a46152c55247da0ae0f008e3e35baaf0e3dee05
costajob <costajob@gmail.com>
2018-04-10 16:25:04 +0200

    Mike - classify japronto app

--- servers/japronto_server.py
+++ servers/japronto_server.py
@@ -1,10 +1,17 @@
+from sys import argv
 from japronto import Application
 
 
-def hello(request):
-    return request.Response(text='Hello World')
+class App(Application):
+    def __init__(self):
+        super().__init__()
+        self.router.add_route('/', self._handle)
+
+    def _handle(self, request):
+        return request.Response(text='Hello World')
 
 
-app = Application()
-app.router.add_route('/', hello)
-app.run(port=9292, debug=False)
+if __name__ == '__main__':
+    app = App()
+    workers = int(argv[1]) if len(argv) > 1 else None
+    app.run(port=9292, worker_num=workers)
